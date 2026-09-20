176fb0ef048abb168f882dbe92c34e883b2c81c4
Abdullah Omar Khan <abdullahomarkhan.786786@gmail.com>
2026-09-20 12:07:51 +0530

    Server setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello1")
+	//create our own router so only these routes which we create using it can be accessed
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json") //header is just a key value pair and whenever responese ends it sends all the headers
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte((`{"status":"ok"}`))) //sends the headers flushes to response
+	})
+
+	//initialise server with configurations
+	srv := http.Server{
+		Addr:         ":8090",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	err := srv.ListenAndServe() //we pass our custom config here
+	if err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
